b09bef6faf643f46a013c35aafaa85c8ff14f453
Jaime Molina <js.molina13@uniandes.edu.co>
2018-07-01 22:33:28 -0500

    Update IsBanarySearchTree.py

--- IsBanarySearchTree.py
+++ IsBanarySearchTree.py
@@ -5,39 +5,16 @@ class node:
         self.left = None
         self.right = None
 """
-class NotBSTError(Exception):
-    def __init__(self, message, errors):
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-        # Now for your custom code...
-        self.errors = errors
+import math
 
-def check_sub_left_tree(left_node, root, right_node):
-    if (left_node):
-        if root.data > left_node.data:
-            check_sub_left_tree(left_node.left, left, left_node.right_node)
-            check_sub_right_tree(left_node.left, left, left_node.right_node)
-        else:
-            raise NotBSTError("False", Exception)
-    else 
-        return 
-
-def check_sub_right_tree(left_node, root, right_node):
-    if (left_right):
-        if root.data < right_node.data:
-            check_sub_left_tree(left_node.left, left, left_node.right_node)
-            check_sub_right_tree(left_node.left, left, left_node.right_node)
-        else:
-            raise NotBSTError("False", Exception)
-    else 
-        return 
+def checkBST(root, Min = 0, Max = 10000):
+    if root == None:
+        return True
+    elif (root.data < Min or root.data > Max):
+        # print("min max root {}".format(root.data))
+        
+        return False
+    # print("root {}".format(root.data))
+        
     
-def checkBST(root):
-    # print("root  ",format(root.data))
-    # print("left  ",format(root.left.left.left))
-    # print("right ",format(root.right.data))
-    try:
-        check_sub_left_tree(root.left, root, root.right)
-        return "True"        
-    except NotBSTError:
-        return "False"
+    return checkBST(root.left, Min, root.data - 1) and checkBST(root.right, root.data + 1, Max)
